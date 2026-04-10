ca0dbf9952b91c2d147533f7b12cfeb0a8add4f0
lens161 <lennart.sack@icloud.com>
2026-04-10 15:07:17 +0200

    Reapply "change compose files to work with mac networking, infra: add volume to pg"
    This reverts commit 9e3f7d3ae6842f1061d4d17a8349a482fcf7f0fc.

---
+++ start-all.sh
@@ -0,0 +1,5 @@
+cd "$(dirname "$0")/infra" && docker compose up -d
+sleep 2
+cd "../core" && docker compose up -d
+sleep 2
+cd "../pool" && docker compose up -d
---
+++ stop-all.sh
@@ -0,0 +1,3 @@
+cd "$(dirname "$0")/infra" && docker compose down
+cd "../core" && docker compose down 
+cd "../pool" && docker compose down 
