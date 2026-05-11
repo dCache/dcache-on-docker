d5d76f404c536f9ad760907ae81ef7048c2c126f
Lennart Sack <lennart.sack@icloud.com>
2026-05-11 11:44:19 +0200

    add psu setup for zone filtering (write through webdav only)

--- stop-all.sh
+++ stop-all.sh
@@ -1,4 +1,4 @@
-cd "$(dirname "$0")/infra" && docker compose down
-cd "../core" && docker compose down 
-cd "../pool" && docker compose down 
+cd "$(dirname "$0")/pool" && docker compose down
 cd "../remote" && docker compose down 
+cd "../infra" && docker compose down 
+cd "../core" && docker compose down 
